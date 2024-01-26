fcca75221717cad892710c6e1d73ce4c3a3882d5
blacf0r7 <93528309+blacf0r7@users.noreply.github.com>
2024-01-26 17:59:46 +1000

    Add files via upload

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,47 +1,42 @@
-// alert(mane1);
-// let age = prompt("Введите возвраст", 18);
-// console.log(85 + 96, "1" + 1, 1 + +"2", "1" + "3", 3 / "3");
-let name = "Arkadiy";
-let age = 64;
-let boolean = true; //false
-let pusto_poka_chto = null; 
-let nichego = undefined;
-let people = {
-    name: "kto tam",
-    age: 18,
-    ljet: true,
-    children: {},
-};
-
-
-
-
-
-
-
-
-
-
-let h1 = document.getElementsByTagName("h1");
-console.log(h1)
-let h2Mass = document.getElementsByTagName("h3");
-console.log(h2Mass)
-
-h2Mass[0].innerText = "новый заголовок";
-
-let classElem = document.getElementsByClassName("header_menu-item");
-
-classElem[classElem.length - 1].innerHTML = "Вставной текст";
-
 let work = document.getElementById("work");
-let education = document.getElementById("education");
- console.log(work, education);
+let education = document.querySelector("education");
+let list = document.querySelector('.qualific__list');
 
-work.onclick = function () {
-    education.style.display = "flex";
-};
-education.onclick = () => {
-    
+const massCards = [
+    {
+    name:"Пограммист",
+    univer:"ПТУ имени Ленина",
+    date:"2000 - 2010",
+    },
+    {
+    name:"Пограммист2",
+    univer:"МГУ",
+    date:"2010 - 2014",
+    },
+    {
+    name:"Пограммист3",
+    univer:"МГИМО",
+    date:"2015 - 2017",
+    },
+];
+
+// let card = {
+//     name:"Пограммbст",
+//     univer:"ПТУ имени Ленина",
+//     date:"2000 - 2010",
+// };
+
+for (let i = 0; i < massCards.length; i++){
+    list.insertAdjacentHTML("afterbegin",`<article class="qualific__item">
+                <h5>${massCards[i].name}</h5>
+                <div class="qualific__item-bottom">
+                  <p>${massCards[i].univer}</p>
+                  <div class="qualific-item__data">
+                    <img src="" alt="" />
+                    <h6>${massCards[i].date}</h6>
+                  </div>
+                </div>
+              </article>`)
 }
-let a  = document.querySelectorAll("nav h3");
-console.log(a); 
+    
+work.onclick = function () {};
